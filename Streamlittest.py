09ccd257e710831091209698d66d607821aa1001
Thomas8932 <114658929+Thomas8932@users.noreply.github.com>
2022-10-31 11:11:57 +0100

    Add files via upload

---
+++ Streamlittest.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 15:02:46 2022
+
+@author: lpnnr
+"""
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+st.header("asdfgfdsa")
