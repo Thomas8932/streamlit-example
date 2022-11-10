52b185782204073b4b75a9a4f808288376d7d4c9
Thomas8932 <114658929+Thomas8932@users.noreply.github.com>
2022-11-10 11:37:42 +0100

    Update streamlittest.py

--- streamlittest.py
+++ streamlittest.py
@@ -63,7 +63,7 @@ for _, row in sharktotal1.iterrows():
 
 
 coords_belgium=[30, 10]
-timelapsemap = folium.Map(location=coords_belgium, control_scale=True, zoom_start=1)
+timelapsemap = folium.Map(location=coords_belgium, control_scale=True, zoom_start=1.2)
 
 
 TimestampedGeoJson(
