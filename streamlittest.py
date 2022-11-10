d3813c8ab08c16871ecdcb299c80d711c5e65de9
Thomas8932 <114658929+Thomas8932@users.noreply.github.com>
2022-11-10 12:55:18 +0100

    Update streamlittest.py

--- streamlittest.py
+++ streamlittest.py
@@ -26,25 +26,7 @@ from folium.plugins import TimestampedGeoJson
 
 
 
-sharktotal1 = pd.read_csv('sharktotal1.csv')
-
-sharktotal1 = sharktotal1[sharktotal1['Year'] > 1969]
-sharktotal1['Year'] = sharktotal1['Year'].replace(3019.0,2019.0)
-sharktotal1['Year'] = pd.to_datetime(arg=sharktotal1['Year'],format="%Y")
-
-sharktotal1 = sharktotal1[sharktotal1['LAT'] != 'geen locatie bekend']
-sharktotal1 = sharktotal1[sharktotal1['LAT'] != 'error']
-sharktotal1 = sharktotal1[sharktotal1['LAT'] != 'alleen land bekend']
-sharktotal1 = sharktotal1[sharktotal1['LAT'] != '1 van de 2 is nan']
-sharktotal1 = sharktotal1[sharktotal1['LAT'] != 'geen locatie bekend']
-sharktotal1 = sharktotal1[sharktotal1['LAT'] != 'error']
-
-sharktotal1[['LAT','LONG']] = sharktotal1[['LAT','LONG']].astype(float)
-
-
-
-
-
+sharktotal1 = pd.read_csv('timelapsedataframe.csv')
 features = []
 for _, row in sharktotal1.iterrows():
     feature = {
@@ -67,19 +49,7 @@ for _, row in sharktotal1.iterrows():
     }
     features.append(feature)
 
-
-
-coords_belgium=[30, 10]
-timelapsemap = folium.Map(control_scale=True, zoom_start=1,
-               location=[20, 10],
-               width=520,
-               height=400)
-
-
-
-
-
-
+timelapsemap = folium.Map(control_scale=True, zoom_start=1,location=[20, 10],width=520,height=400)
 
 TimestampedGeoJson(
     {'type': 'FeatureCollection',
@@ -89,19 +59,10 @@ TimestampedGeoJson(
     , auto_play=False
     , loop=False
     , max_speed=1
-    , loop_button=True
+    , loop_button=False
     , date_options='YYYY'
     , time_slider_drag_update=True
 ).add_to(timelapsemap)
-
-
-#timelapsemap
-
-# sharkjason = create_geojson_features(sharktotal1)
-# make_map(sharkjason)
-
-
-
 
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
