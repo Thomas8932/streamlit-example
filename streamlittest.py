3dae7d3419bf815acad58cdd5c9b216b0a061786
Thomas8932 <114658929+Thomas8932@users.noreply.github.com>
2022-11-11 11:09:29 +0100

    Update streamlittest.py

--- streamlittest.py
+++ streamlittest.py
@@ -226,21 +226,7 @@ with tab3:
   st.plotly_chart(fig3)
   
   ###############################################################################################################################################
-  
-  fig4 = px.scatter(display_box,x= 'Year', y='Attack', color='Fatal (Y/N)', trendline='ols',
-                   labels={
-                       "Year": "Jaar",
-                       "Attack": "Aantal aanvallen"
-                  },
-                  title='Aantal aanvallen per jaar')
-  fig4.update_layout(legend_title="Uitkomst aanval")
-  fig4.for_each_trace(lambda t: t.update(name = newnames[t.name],
-                                        legendgroup = newnames[t.name],
-                                        hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])
-                                       )
-                    )
-  fig4.update_xaxes(rangeslider_visible=True)
-  st.plotly_chart(fig4)  
+
   
 ###############################################################################################################################################
   
