99fc5a87642feebeb5763d3c31d60828e8789610
Thomas8932 <114658929+Thomas8932@users.noreply.github.com>
2022-11-11 11:40:08 +0100

    Update streamlittest.py

--- streamlittest.py
+++ streamlittest.py
@@ -354,7 +354,7 @@ with tab3:
   fig4.update_layout(legend_title="Uitkomst aanval")
   
   fig4.for_each_trace(lambda t: t.update(name = newnames[t.name],legendgroup = newnames[t.name],hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])))
-  fig4.update_traces(symbol="diamond")
+#  fig4.update_traces(symbol="diamond")
   fig4.update_xaxes(rangeslider_visible=True)
   st.plotly_chart(fig4)    
   
